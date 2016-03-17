196530e251118c78b4a92565d9f68f95301c6eff
KrzMac <kwmacioszek@gmail.com>
2016-03-17 16:20:42 +0100

    version 0.0.1

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,14 @@
+import java.io.IOException;
+
 /**
  * Created by KMacioszek on 2016-03-17.
  */
 public class main {
 
-    public static void Main(String[] arg) {
-
+    public static void main(String args[]) throws IOException {
+        if (args.length != 1)
+            throw new RuntimeException ("Syntax: ChatServer <port>");
+        new ChatServer (Integer.parseInt (args[0]));
     }
 
 }
